bf79e320cec6f0be6239e787253effde5ad74f40
Suraj Yadav <surajy1303@gmail.com>
2026-03-01 13:38:48 +0530

    added the controller for authorization

--- src/main/java/com/url/shortener/security/jwt/JwtUtils.java
+++ src/main/java/com/url/shortener/security/jwt/JwtUtils.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -14,7 +15,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-@Service
+@Component
 public class JwtUtils {
 
     @Value("${jwt.secret}")
